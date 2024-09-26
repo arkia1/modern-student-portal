214b6f235f111d8d1d48eb371d6660e0e3f1f507
arkia1 <arkia.ebrahimi84@gmail.com>
2024-09-26 13:21:57 +0300

    set up

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+const App = () => {
+  return (
+    <div>
+      <h2 className="text-indigo-600 text-center font-bold text-xl">
+        hello dear student
+      </h2>
+    </div>
+  );
+};
+
+export default App;
